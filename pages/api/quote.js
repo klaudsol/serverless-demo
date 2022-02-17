54faad517deeb01208c51a94bc527ae71dd82f08
Ardee Aram <ardee.aram@safelinkhub.com>
2022-02-17 14:17:04 +0000

    Test

--- pages/api/quote.js
+++ pages/api/quote.js
@@ -1,15 +1,5 @@
-import AWS from 'aws-sdk';
-
-
 
 export default function handler(req, res) {
-  const {
-    AURORA_AWS_ACCESS_KEY_ID,
-    AURORA_AWS_SECRET_ACCESS_KEY,
-    AURORA_RESOURCE_ARN,
-    AURORA_SECRET_ARN,
-    AURORA_DATABASE
-  } = process.env;
 
   const quotes = [
     "Lorem",
